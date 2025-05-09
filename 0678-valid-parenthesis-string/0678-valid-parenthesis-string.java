58aebaa040f28d019d8fa50f7872eccde1ee860c
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-05-09 17:15:07 +0530

    Time: 0 ms (100%), Space: 41.1 MB (98.21%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,47 +1,31 @@
-class Solution 
-{
-    public boolean checkValidString(String s) 
-    {
-        int open_stack[]=new int[s.length()];
-        int open_top=-1;
-        int star_stack[]=new int[s.length()];
-        int star_top=-1;
+class Solution {
+    public boolean checkValidString(String s) {
+        int min_open=0;
+        int max_open=0;
 
-        for(int i=0;i<s.length();i++)
+        for(int i=0 ; i<s.length() ; i++)
         {
             if(s.charAt(i)=='(')
             {
-                open_stack[++open_top]=i;
+                min_open++;
+                max_open++;
             }
-            else if(s.charAt(i)=='*')
+            else if(s.charAt(i)==')')
             {
-                star_stack[++star_top]=i;
-            }
-            else //if the closing bracket occurs
-            {
-                if(open_top>-1)//if the open_stack has the values
-                    open_top--;
-                else if(star_top>-1)
-                    star_top--;
-                else
-                    return false;
-            }
-        }
-        if(open_top==-1)//means all open brackets are balanced.star_stack is not considered.because even it has values.it considered as empty strings
-           return true;
-        //the below block will be executed when the open_stack has values
-        while(open_top>-1 && star_top>-1)
-        {
-            if(open_stack[open_top]<star_stack[star_top])
-            {
-                open_top--;
-                star_top--;
+                if(min_open>0)
+                   min_open--;
+                max_open--;
             }
             else
             {
-                return false;
+                if(min_open>0)
+                   min_open--; //treated * as ) or ""
+                max_open++;    //treated * as (
             }
-        }       
-        return open_top==-1;
+            if(max_open<0) 
+               return false;
+            
+        }
+        return min_open==0;
     }
 }
