c672341b4703bbed3543760212b1dcebfa877a6d
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-05-20 23:07:40 +0530

    Time: 0 ms (100%), Space: 43.4 MB (18.94%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,15 +4,12 @@ class Solution
     {
         int low=0;
         int high=nums.length-1;
-        int insert_position=-1;
 
         while(low<=high)
         {
             int mid=low+(high-low)/2;
 
-            if(nums[mid]==target)
-               return mid;
-            else if(nums[mid]<target)
+            if(nums[mid]<target)
                low=mid+1;
             else
                high=mid-1;
