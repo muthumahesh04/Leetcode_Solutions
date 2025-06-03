37630c8a8a5ef037f9071024eb0f9074275f53a9
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-06-03 23:31:03 +0530

    Time: 1 ms (99.74%), Space: 45.8 MB (81.01%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -2,26 +2,41 @@ class Solution
 {
     public void moveZeroes(int[] nums) 
     {
-        int zeropointer=0;
-        int nonzeropointer=0;
+        // int zeropointer=0;
+        // int nonzeropointer=0;
 
-        while(nonzeropointer<nums.length)
+        // while(nonzeropointer<nums.length)
+        // {
+        //     if(nums[zeropointer]!=0)
+        //     {
+        //         zeropointer++;
+        //         nonzeropointer++;
+        //     }
+        //     else if(nums[nonzeropointer]==0)
+        //     {
+        //         nonzeropointer++;
+        //     }
+        //     else
+        //     {
+        //         int temp=nums[zeropointer];
+        //         nums[zeropointer]=nums[nonzeropointer];
+        //         nums[nonzeropointer]=temp;
+        //     }
+        // }
+
+        int k=0;
+
+        for(int i=0 ;i<nums.length ;i++)
         {
-            if(nums[zeropointer]!=0)
+            if(nums[i]!=0)
             {
-                zeropointer++;
-                nonzeropointer++;
+                nums[k]=nums[i];
+                k++;
             }
-            else if( nums[nonzeropointer]==0)
-            {
-                nonzeropointer++;
-            }
-            else
-            {
-                int temp=nums[zeropointer];
-                nums[zeropointer]=nums[nonzeropointer];
-                nums[nonzeropointer]=temp;
-            }
+        }
+        for(int j=k ;j<nums.length ;j++)
+        {
+            nums[j]=0;
         }
     }
 }
