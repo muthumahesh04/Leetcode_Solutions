2cfe54de2ef4b494479b3c102b76b599dc593c97
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-06-03 23:24:43 +0530

    Time: 2 ms (81.95%), Space: 46.3 MB (11.2%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public void moveZeroes(int[] nums) 
+    {
+        int zeropointer=0;
+        int nonzeropointer=0;
+
+        while(nonzeropointer<nums.length)
+        {
+            if(nums[zeropointer]!=0)
+            {
+                zeropointer++;
+                nonzeropointer++;
+            }
+            else if(nonzeropointer<nums.length && nums[nonzeropointer]==0)
+            {
+                nonzeropointer++;
+            }
+            else
+            {
+                int temp=nums[zeropointer];
+                nums[zeropointer]=nums[nonzeropointer];
+                nums[nonzeropointer]=temp;
+            }
+        }
+    }
+}
