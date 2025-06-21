43f518cb4eab0c15ded763dccc5661d5266ccd1b
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-06-21 12:54:08 +0530

    Time: 3 ms (90.15%), Space: 45 MB (62.52%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,15 +1,22 @@
-class Solution {
-    public String convert(String s, int numRows) {
-        if (numRows == 1) return s;
-        StringBuilder a = new StringBuilder();
-        for(int i = 0; i < numRows; i++){
-            for(int j = i; j< s.length(); j+= (2 * (numRows - 1))){
-                a.append(s.charAt(j));
-                if(i > 0 && i < numRows - 1 && j + (2 * (numRows - 1)) -(2 * i) < s.length()){
-                    a.append(s.charAt(j + (2* (numRows - 1)) - (2 * i)));
-                }
+class Solution 
+{
+    public String convert(String s, int numRows) 
+    {
+        StringBuilder result=new StringBuilder();
+
+        if(numRows==1)
+            return s;
+        for(int i=0 ; i<numRows ; i++)
+        {
+            for(int j=i ; j<s.length() ; j+=(numRows-1)*2)
+            {
+                result.append(s.charAt(j));
+
+                //for the middle rows, append the middle characters
+                if(i>0 && i<numRows-1 && j+((numRows-1)*2)-(i*2)<s.length())
+                    result.append(s.charAt(j+((numRows-1)*2)-(i*2)));
             }
         }
-        return a.toString();
+        return result.toString();
     }
 }
