fd00389ffa0d82ba934784ad4a6f346d6d051ff4
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-06-22 19:40:13 +0530

    Time: 4 ms (94.55%), Space: 42.4 MB (98.84%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -7,7 +7,6 @@ class Solution {
       reverse(ch,0,n-1);
       reverseWord(ch,n);
       return cleanSpace(ch,n);
-
     }
 
     private void reverse(char[] ch,int i,int j){
@@ -20,26 +19,26 @@ class Solution {
     }
 
      private void reverseWord(char[] ch,int n){
-        int i=0,j=0;
-        while(j<n){
-            while(j<n && ch[j]==' ') j++;   // Skip spaces
-            i=j;                            // Set i to the start of the word
-            while(j<n && ch[j]!=' ') j++;   //j points to space next to end of a word
-            reverse(ch,i,j-1);
+        int wordstart=0,traversing_pointer=0;
+        while(traversing_pointer<n){
+            while(traversing_pointer<n && ch[traversing_pointer]==' ') traversing_pointer++;   // Skip spaces
+            wordstart=traversing_pointer;                            // Set i to the start of the word
+            while(traversing_pointer<n && ch[traversing_pointer]!=' ') traversing_pointer++;   //j points to space next to end of a word
+            reverse(ch,wordstart,traversing_pointer-1);
         }
      }
 
     // Shift characters forward and ensure only one space between words
      private String cleanSpace(char[] ch,int n){
-        int i=0,j=0;
-        while(j<n){
-            while(j<n && ch[j]==' ') j++;
-            while(j<n && ch[j]!=' ') ch[i++]=ch[j++];
-            while(j<n && ch[j]==' ') j++;
-            if(j<n) ch[i++]=' ';            // Ensure no space is left after the last word (only add space if j < n)
+        int writing_pointer=0,traversing_pointer=0;
+        while(traversing_pointer<n){
+            while(traversing_pointer<n && ch[traversing_pointer]==' ') traversing_pointer++;
+            while(traversing_pointer<n && ch[traversing_pointer]!=' ') ch[writing_pointer++]=ch[traversing_pointer++];
+            while(traversing_pointer<n && ch[traversing_pointer]==' ') traversing_pointer++;
+            if(traversing_pointer<n) ch[writing_pointer++]=' ';            // Ensure no space is left after the last word (only add space if j < n)
 
         }
-          return new String(ch).substring(0,i);    //after all forward swaps i will be pointing to the last character
+          return new String(ch).substring(0,writing_pointer);    //after all forward swaps i will be pointing to the last character
      }
     
 }
