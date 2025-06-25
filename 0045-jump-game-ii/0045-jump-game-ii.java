3a45edbaff6e6e6d9755cc3ac0155bb18dc7f20e
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-06-25 20:58:52 +0530

    Time: 1 ms (99.41%), Space: 44.7 MB (97.41%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,27 @@
+class Solution 
+{
+    public int jump(int[] nums) 
+    {
+        int l=0;
+        int r=0;
+        int minimum_jumps=1;
+        int maximum_coverage=0;
+
+        while(r<nums.length-1)
+        {
+            for(int i=l;i<=r;i++)
+            {
+                maximum_coverage=Math.max(maximum_coverage,i+nums[i]);
+            }
+            if(maximum_coverage<=r)
+               return -1;//we can't reach the end
+            if(maximum_coverage>=nums.length-1)
+              return minimum_jumps;
+            
+            minimum_jumps++;
+            l=r+1;
+            r=maximum_coverage;
+        }
+        return 0;
+    }
+}
