17ed040ec7311b95b35e39e7af2028ac0c42336a
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-06-26 21:36:55 +0530

    Time: 3 ms (91.1%), Space: 42.9 MB (88.66%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public boolean isAnagram(String s, String t) 
+    {
+
+        if(s.length()!=t.length())
+             return false;
+        
+        int count_frequency[]=new int[26];
+        
+        for(int i=0;i<s.length();i++)
+        {
+            count_frequency[s.charAt(i)-'a']++;
+        }
+        for(int j=0;j<t.length();j++)
+        {
+            if(count_frequency[t.charAt(j)-'a']==0)
+               return false;
+            count_frequency[t.charAt(j)-'a']--;
+        }
+        for(int i=0;i<count_frequency.length;i++)
+        {
+            if(count_frequency[i]!=0)
+              return false;
+        }
+        return true;
+
+    }
+}
