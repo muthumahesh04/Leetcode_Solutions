da16c8c863110d9cfe1979219fb083459246b796
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-06-29 21:52:40 +0530

    Time: 0 ms (100%), Space: 43 MB (66.19%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -9,11 +9,13 @@ class Solution
         {
             int mid=low+(high-low)/2;
 
-            if(nums[mid]<target)
+            if(nums[mid]==target)
+               return mid;
+            else if(nums[mid]<target)
                low=mid+1;
             else
                high=mid-1;
-        }        
+        }
         return low;
     }
 }
