4f79de4752b127b7391ad70e8145703d9af18665
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-06-29 21:12:34 +0530

    Time: 1 ms (60.1%), Space: 45.2 MB (12.42%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,13 +4,13 @@ class Solution
     {
         int write_index=1;
 
-        for(int read_index=0; read_index<nums.length ; read_index++)
+        for(int read_index=1; read_index<nums.length ; read_index++)
         {
             if(nums[read_index]!=nums[write_index-1])
             {
-                    nums[write_index]=nums[read_index];
-                    write_index++;
-            }
+                nums[write_index]=nums[read_index];
+                write_index++;
+            }   
         }
         return write_index;
     }
