25e438a3f594a92a1c2318da5633f3b87ac3e8df
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-07-10 00:34:59 +0530

    Time: 15 ms (5.62%), Space: 46 MB (11.76%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -2,8 +2,46 @@ class Solution
 {
     public int maximumProduct(int[] nums) 
     {
+        // Arrays.sort(nums);
+        // int length=nums.length;
+        // return Math.max(nums[length-1]*nums[length-2]*nums[length-3] , nums[length-1]*nums[0]*nums[1]);
+
         Arrays.sort(nums);
-        int length=nums.length;
-        return Math.max(nums[length-1]*nums[length-2]*nums[length-3] , nums[length-1]*nums[0]*nums[1]);
+
+        int max=Integer.MIN_VALUE;
+        int second_max=Integer.MIN_VALUE;
+        int third_max=Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
+        int second_min=Integer.MAX_VALUE;
+
+        for(int i=0 ; i<nums.length ; i++)
+        {
+            if(nums[i]>max)
+            {
+                third_max=second_max;
+                second_max=max;
+                max=nums[i];
+            }
+            else if(nums[i]>second_max)
+            {
+                third_max=second_max;
+                second_max=nums[i];
+            }
+            else if(nums[i]>third_max)
+            {
+                third_max=nums[i];
+            }
+
+            if(nums[i]<min)
+            {
+                second_min=min;
+                min=nums[i];
+            }
+            else if(nums[i]<second_min)
+            {
+                second_min=nums[i];
+            }
+        }
+        return Math.max(max*second_max*third_max, min*second_min*max);
     }
 }
