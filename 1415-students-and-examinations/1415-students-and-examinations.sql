6339a5435f2e191f2a3d27a399cd7bd0b3c41dd3
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-07-21 01:08:16 +0530

    Time: 909 ms (56.53%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -2,7 +2,7 @@
 select s.student_id,s.student_name,sub.subject_name,count(e.subject_name) as attended_exams #this e.subject_name avoids the null.
 from Students s cross join Subjects sub
 left join Examinations e #we are left joing with the cross joined table.
-on s.student_id=e.student_id and sub.subject_name=e.subject_name
+on s.student_id=e.student_id and sub.subject_name=e.subject_name #here sub refers to cross joined table's 
 group by s.student_id,s.student_name,sub.subject_name 
 --  1          | Alice        | Math         
 -- | 1          | Alice        | Math         
