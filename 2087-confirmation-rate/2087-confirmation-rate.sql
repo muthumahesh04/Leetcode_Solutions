e1cbd2ab2716167bc24d4b09fe46822ad6fb48d9
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-07-21 23:45:30 +0530

    Time: 689 ms (35.9%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -2,7 +2,7 @@
 #why i taken this column is, we have to count total no of requests at the same time we have to group.so 
 with total_and_confirmed as (
 select s.user_id,
-       count(s.user_id) as total_requests,
+       count(c.user_id) as total_requests,
        sum(case when c.action='confirmed' then 1 else 0 end) as confirmed_requests
 from 
        Signups s
@@ -14,4 +14,4 @@ group by s.user_id
 )
 
 select user_id,ifnull(round(confirmed_requests/total_requests,2),0) as confirmation_rate
-from total_and_confirmed; 
+from total_and_confirmed;
