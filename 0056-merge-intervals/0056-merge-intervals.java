05dd6f962b56a4307fb72f6868ca07e8df38c82e
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-07-25 15:35:18 +0530

    Time: 280 ms (5.49%), Space: 46.6 MB (72.41%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,51 +1,41 @@
-class Solution 
-{
-    public void sort_intervals(int[][] arr)
-    {
-        int length=arr.length;
-        for(int i=0 ; i<length ; i++)
-        {
-            for(int j=0 ; j<length-i-1 ; j++)
-            {
-                if(arr[j][0]>arr[j+1][0])
-                {
-                    int[] temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+class Solution {
+    public void sort_intervals(int[][] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length - i - 1; j++) {
+                if (arr[j][0] > arr[j + 1][0]) {
+                    int[] temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
-    public int[][] merge(int[][] intervals) 
-    {
+
+    public int[][] merge(int[][] intervals) {
         sort_intervals(intervals);
-        
-        List<int[]> result=new ArrayList<>();
 
-        int start=intervals[0][0];
-        int end=intervals[0][1];
+        List<int[]> result = new ArrayList<>();
 
-        for(int i=0 ; i<intervals.length ; i++)
-        {
-            if(intervals[i][0]<=end)
-            {
-                end=Math.max(end,intervals[i][1]);
-            }
-            else
-            {
-                result.add(new int[]{start,end});
-                start=intervals[i][0];
-                end=intervals[i][1];
+        int start = intervals[0][0]; //Initialize with the starting index's first number
+        int end = intervals[0][1];  //Inintialize with the starting index's second number
+
+        for (int i = 0; i < intervals.length; i++) {
+            if (intervals[i][0] <= end) {
+                end = Math.max(end, intervals[i][1]);
+            } else {
+                result.add(new int[] { start, end });
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
-        result.add(new int[]{start,end});
+        result.add(new int[] { start, end });
 
-        int answer[][]=new int[result.size()][];
-        for(int i=0 ; i<result.size() ; i++)
-        {
-            answer[i]=result.get(i);
+        int answer[][] = new int[result.size()][];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
         }
         return answer;
     }
-    
+
 }
