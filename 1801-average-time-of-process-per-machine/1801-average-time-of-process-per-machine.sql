f81f90f41f259eb8dcc87a9efb22869402c0c867
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-07-31 22:58:07 +0530

    Time: 399 ms (7.42%), Space: 0B (100%) - LeetHub

--- 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -1,14 +1,22 @@
-# Write your MySQL query statement below
-with processing_Time AS (
-    select a.machine_id,(b.timestamp-a.timestamp) as processtime
-    from Activity a inner join Activity b
-    on a.machine_id=b.machine_id and
-    a.process_id=b.process_id and
-    a.activity_type="start" and b.activity_type="end"
-)
-
-select machine_id,
-       round(avg(processtime),3) as processing_time
-from processing_Time
-group by machine_id;
-
+-- # Write your MySQL query statement below
+-- with processing_time_per_machine AS 
+-- (
+--     select a.machine_id,round(AVG(sum(b.timestamp-a.timestamp)),3) as processing_time
+--     from Activity a inner join Activity b
+--     on 
+--        a.process_id=b.process_id and
+--        a.activity_type='start' and
+--        b.activity_type='end'
+-- )
+-- select machine_id,processing_time
+-- from processing_time_per_machine
+-- group by machine_id;
+SELECT a.machine_id, 
+       ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a
+JOIN Activity b 
+  ON a.process_id = b.process_id 
+ AND a.machine_id = b.machine_id
+AND a.activity_type = 'start' 
+  AND b.activity_type = 'end'
+GROUP BY a.machine_id;
