e685f48a50bbe59e86d6a7e262867b6a96d885ad
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-09-09 17:23:16 +0530

    Time: 17 ms (5.59%), Space: 43.4 MB (90.88%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -10,8 +10,9 @@ class Solution {
     }
     public void Subsets(int arr[],int index,List<List<Integer>> result,List<Integer> tempList)
     {
-        
-        if(index==arr.length)
+        if(result.contains(tempList))
+           return;
+        if(index==arr.length && !result.contains(tempList))
         {
             result.add(new ArrayList<>(tempList));
             return;
@@ -19,7 +20,7 @@ class Solution {
         tempList.add(arr[index]);
         Subsets(arr,index+1,result,tempList);
         tempList.remove(tempList.size()-1);
-        while(index<arr.length-1 && arr[index]==arr[index+1]) index++;
+        
         Subsets(arr,index+1,result,tempList);
     }
 }
