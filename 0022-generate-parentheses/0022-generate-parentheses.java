c3c9cab85880b19b6b4c5ab06374daacc2e96812
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-09-09 18:07:23 +0530

    Time: 1 ms (81.52%), Space: 42.4 MB (99.62%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,26 @@
-class Solution {
-    public List<String> generateParenthesis(int n) {
+class Solution 
+{
+    public List<String> generateParenthesis(int n) 
+    {
         List<String> result=new ArrayList<>();
-
-        Generate_Parantheses("",result,0,0,n);
+        StringBuilder sb=new StringBuilder();
+        Generate_Parantheses(sb,result,0,0,n);
 
         return result;
     }
-    public void Generate_Parantheses(String str,List<String> result,int open,int close,int limit)
+    public void Generate_Parantheses(StringBuilder str,List<String> result,int open,int close,int limit)
     {
         if(open==limit && close==limit)
         {
-            result.add(str);
+            result.add(str.toString());
             return;
         }
         if(open>limit || close>open)
             return;
         
-        Generate_Parantheses(str+"(",result,open+1,close,limit);
-        Generate_Parantheses(str+")",result,open,close+1,limit);
+        Generate_Parantheses(str.append("("),result,open+1,close,limit);
+        str.deleteCharAt(str.length()-1);
+        Generate_Parantheses(str.append(")"),result,open,close+1,limit);
+        str.deleteCharAt(str.length()-1);
     }
 }
