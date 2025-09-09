b001d6bed37794cdf7ee18d39ee37f514a4dae35
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-09-09 17:14:16 +0530

    Time: 602 ms (5.24%), Space: 45.3 MB (34.09%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,37 @@
+class Solution 
+{
+    public List<List<Integer>> permuteUnique(int[] nums) 
+    {
+        List<List<Integer>> result=new ArrayList<>();
+        boolean[] isTaken=new boolean[nums.length];
+
+        Arrays.sort(nums);
+        GeneratePermutations(result,new ArrayList<>(),nums,isTaken);
+
+        return result;
+    }
+    public void GeneratePermutations(List<List<Integer>> result,List<Integer> tempList,int nums[],boolean[] isTaken)
+    {
+        if(tempList.size()==nums.length && !result.contains(tempList))
+        {
+            result.add(new ArrayList<>(tempList));
+            return;
+        }
+
+        for(int i=0 ; i<nums.length ; i++)
+        {
+            // if(tempList.contains(num))
+            //    continue;
+            if(isTaken[i])
+              continue;
+            isTaken[i]=true;
+            tempList.add(nums[i]);
+
+            GeneratePermutations(result,tempList,nums,isTaken);
+
+            isTaken[i]=false;
+            tempList.remove(tempList.size()-1);
+        }
+    }
+}
+
