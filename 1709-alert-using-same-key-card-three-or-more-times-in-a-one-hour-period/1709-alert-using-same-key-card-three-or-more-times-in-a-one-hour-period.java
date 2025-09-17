74c80243562b9aea033512afef694257cf253246
Muthu Mahesh <146420125+muthumahesh04@users.noreply.github.com>
2025-09-17 21:44:57 +0530

    Time: 47 ms (89.42%), Space: 63.5 MB (84.38%) - LeetHub

--- 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.java
+++ 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.java
@@ -1,13 +1,11 @@
 class Solution 
 {
-    public int processTime(String time)
+    public int ProcessTime(String time)
     {
         int Hours=Integer.parseInt(time.substring(0,2));
         int Minutes=Integer.parseInt(time.substring(3,5));
 
-        int totalMinutes=(Hours*60)+Minutes;
-
-        return totalMinutes;
+        return (Hours*60)+Minutes;
     }
     public List<String> alertNames(String[] keyName, String[] keyTime) 
     {
@@ -15,31 +13,26 @@ class Solution
 
         for(int i=0 ; i<keyName.length ; i++)
         {
-            // list.add(...) returns a boolean (true/false),
-            // so you were accidentally storing a boolean instead of a list in the map ❌.
-            // myMap.put(keyName[i],myMap.getOrDefault(name,new ArrayList<>()).add(processTime(keyTime[i])));
-     
-            // myMap.computeIfAbsent("daniel", k -> new ArrayList<>())
-            // Create daniel → [] (an empty list) if "daniel" not in map,
-            // return the existing list myMap.get("daniel") if it’s already there.
-            myMap.computeIfAbsent(keyName[i], k -> new ArrayList<>()).add(processTime(keyTime[i]));
+            myMap.computeIfAbsent(keyName[i], k-> new ArrayList<>()).add(ProcessTime(keyTime[i]));
         }
-        List<String> alerts = new ArrayList<>();
 
-        for (String name : myMap.keySet()) {
-            List<Integer> times = myMap.get(name);
+        List<String> alertNames=new ArrayList<>();
+
+        for(String name: myMap.keySet())
+        {
+            List<Integer> times=myMap.get(name);
             Collections.sort(times);
 
-            for (int j = 2; j < times.size(); j++) {
-                if (times.get(j) - times.get(j - 2) <= 60) {
-                    alerts.add(name);
-                    break; // only add once per worker
+            for(int i=2 ; i<times.size() ; i++)
+            {
+                if(times.get(i)-times.get(i-2)<=60)
+                {
+                    alertNames.add(name);
+                    break;
                 }
             }
         }
-
-        Collections.sort(alerts);
-        return alerts;
-        
+        Collections.sort(alertNames);
+        return alertNames;
     }
 }
